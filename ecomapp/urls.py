272883838aea8a8abd59b3618b90908d1d51f157
shreyank07 <shreyankladekar07@gmail.com>
2021-06-28 13:15:02 +0530

    endpoints for cart and login authentication

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 
-from ecomapp.views import register, fetchusers, login, insert, mens, mensproduct,w,home,homeproduct
+from ecomapp.views import register, fetchusers, login, insertusers,users, mens, mensproduct,w,home,homeproduct,insertitem,citem
 
 urlpatterns = {
     path('register', register),
     path('fetchusers', fetchusers),
     path('login', login),
-    path('insert', insert),
+    path('insertusers', insertusers),
+    path('users', users),
     path('mens', mens),
     path('mensproduct', mensproduct),
     path('w', w),
     path('home', home),
     path('homeproduct', homeproduct),
+    path('insertitem', insertitem),
+    path('citem', citem),
 }
 
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,13 +1,21 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
+from rest_framework.decorators import api_view
+from django.conf import settings
+import jwt
+import datetime
 
 from pymongo import MongoClient
 from django.views.decorators.csrf import csrf_exempt
+
+from ecomapp import utils
 
 client = MongoClient('mongodb://localhost:27017')
 db = client['Product_Info']
 collection = db['Details']
 popular = db['popularWears']
+userobj = db['user']
+cartitem = db['cart']
 
 
 def idconverter():
@@ -23,6 +31,19 @@ def idconverter2():
         u['_id'] = str(u['_id'])
     return wear
 
+
+def idconverter3():
+    userdetail = [i for i in userobj.find()]
+    for u in userdetail:
+        u['_id'] = str(u['_id'])
+    return userdetail
+
+
+def idconverter4():
+    cart = [i for i in cartitem.find()]
+    for u in cart:
+        u['_id'] = str(u['_id'])
+    return cart
 
 def register(request):
     with open('src/db.json', 'r') as f:
@@ -51,20 +72,46 @@ def fetchusers(request):
     return JsonResponse({'status': True, 'users': users})
 
 
+def insertusers(request):
+    userdetail = idconverter3()
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        # userdetail = userobj.insert_one(data)
+        for u in userdetail:
+            if u['email'] == data["email"]:
+                return JsonResponse({'status': False, 'message': 'Email already Exist'})
+            else:
+                userdetail = userobj.insert_one(data)
+                return JsonResponse({'status': True, 'message': 'Login Successfull'})
+
+
+def users(request):
+    userdetail = idconverter3()
+    return JsonResponse({'status': True, 'userdetail': userdetail})
+
+
+@api_view(['POST'])
 def login(request):
-    with open('src/db.json', 'r') as f:
-        users = json.load(f)
-    userdata = json.loads(request.body)
-    for user in users:
-        if user['email'] == userdata['email']:
-            return JsonResponse({'status': True, 'message': 'Login Successfull'})
-        else:
-            return JsonResponse({'status': False, 'message': 'Account not exist'})
-    for user in users:
-        if user['email'] == " ":
-            return JsonResponse({'status': False, 'message': 'fill in the required detail'})
-        if user['password'] == " ":
-            return JsonResponse({'status': False, 'message': 'fill in the required detail'})
+    req_user = json.loads(request.body)
+    userid = userobj.find_one({'email': req_user['email']})
+    if userid['password'] != req_user['password']:
+        return HttpResponse('Unauthorized', status=401)
+
+    token = jwt.encode(
+        {'email': req_user['email'],
+         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=24 * 60 * 60)
+         },
+        settings.SECRET_KEY,
+        algorithm="HS256").decode('utf-8')
+
+    return JsonResponse({'status': True, 'token': token})
+
+
+@utils.requireLogin
+@api_view(['GET'])
+def getdata(request, **kwargs):
+    data = 'some data'
+    return JsonResponse({'status': True, 'data': data})
 
 
 def insert(request):
@@ -110,4 +157,17 @@ def homeproduct(request):
         if (pro['_id'] == data['_id']):
             return JsonResponse({'status': True, 'picture': pro['picture'], 'price': pro['price'],
                                  'warranty': pro['warranty'], 'dimension': pro['dimension'], 'color': pro['color'],
-                                 'weight': pro['weight'],'product': pro['product']})
+                                 'weight': pro['weight'], 'product': pro['product']})
+
+
+def insertitem(request):
+    cart = idconverter4()
+    if request.method == 'POST':
+        data3 = json.loads(request.body)
+        cart = cartitem.insert_one(data3)
+        return JsonResponse({'status': True,'message':'Item Succesfully Added'})
+
+
+def citem(request):
+    cart = idconverter4()
+    return JsonResponse({'status': True, 'cart': cart})
