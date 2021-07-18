91b02de5ba59899efb05aef87a80352f2d743673
VanshilGupta <vanshilgupta28@gmail.com>
2021-07-18 13:54:57 +0530

    Updated men's page

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -11,37 +11,37 @@ export class UserService {
     return [{ name: 'ShreyankLadekar', email: 'Shreyank@autonise.com' }];
   }
   getdata2(category) {
-    return this.http.get('http://127.0.0.1:8000/data', {
+    return this.http.get('http://127.0.0.1:8000/Women/data', {
       params: { category: category },
     });
   }
 
   getProductDetails(category) {
-    return this.http.get('http://127.0.0.1:8000/product', {
+    return this.http.get('http://127.0.0.1:8000/Women/product', {
       params: { category: category },
     });
   }
 
   getUserCart(name) {
-    return this.http.get('http://127.0.0.1:8000/cart', {
+    return this.http.get('http://127.0.0.1:8000/Women/cart', {
       params: { name: name },
     });
   }
   postCart(name,category,id) {
-    return this.http.post('http://127.0.0.1:8000/cart', {"category" : category , "id" : id, "qty" : 1}, {
+    return this.http.post('http://127.0.0.1:8000/Women/cart', {"category" : category , "id" : id, "qty" : 1}, {
       params: { name: name },
     });
   }
 
   getDetailedCart(name){
-    return this.http.get('http://127.0.0.1:8000/cartDetails' , {params : {"name" : name}})
+    return this.http.get('http://127.0.0.1:8000/Women/cartDetails' , {params : {"name" : name}})
   }
   updateQty(name,index,qty){
-    return this.http.post('http://127.0.0.1:8000/updateQty',{"quantity" : qty},{params : {"name" : name, "index" : index}})
+    return this.http.post('http://127.0.0.1:8000/Women/updateQty',{"quantity" : qty},{params : {"name" : name, "index" : index}})
   }
 
   removeCart(name,index){
-    return this.http.get('http://127.0.0.1:8000/removeItem',{
+    return this.http.get('http://127.0.0.1:8000/Women/removeItem',{
       params : {"name" : name, "index" : index}
     })
   }
